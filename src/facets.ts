18af5c9555b88450c5f70b205f1b21801689da5e
Luca Cossaro <luca.cossaro@gmail.com>
2016-02-17 10:52:27 +0100

    added facet base class and start parsing one drive items

--- src/facets.ts
+++ src/facets.ts
@@ -1,56 +1,108 @@
 
-export class Folder {
+class Facet {
+  constructor(data: Object) {
+    for (let key in data) {
+      if (data[key] !== undefined)
+        this[key] = data[key];
+    }
+  }
+}
+
+export class Folder extends Facet{
+  fileCount: number;
+
   constructor(data: any) {
+    super(data);
+  }
+}
+
+export class File extends Facet {
+  hashes: {crc32Hash: string, sha1Hash: string}
+  mimeType: string;
+
+  constructor(data: any) {
+    super(data);
+  }
+}
+
+export class FileSystemInfo extends Facet {
+  createdDateTime: Date;
+  lastModifiedDateTime: Date
+
+  constructor(data: any) {
+    super(data);
+
+    this.createdDateTime = new Date(data.createdDateTime);
+    this.lastModifiedDateTime = new Date(data.lastModifiedDateTime);
+  }
+}
+
+export class Image extends Facet {
+  constructor(data: any) {
+    super(data);
+  }
+}
+
+export class Photo extends Facet {
+  takenDateTime: Date
+
+  constructor(data: any) {
+    super(data);
+    this.takenDateTime = new Date(data.takenDateTime);
+  }
+}
+
+export class Video extends Facet {
+  bitrate: number;
+  duration: number;
+  height:  number;
+  width: number;
+
+  constructor(data: any) {
+    super(data);
+  }
+}
+
+export class Location extends Facet {
+  altitude: number;
+  latitude: number;
+  longitude: number;
+
+  constructor(data: any) {
+    super(data);
 
   }
 }
 
-export class File {
-
-}
-
-export class FileSystemInfo {
-
+export class Remote extends Facet {
   constructor(data: any) {
+    super(data);
 
   }
 }
 
-export class Image {
-
-}
-
-export class Photo {
-
-}
-
-export class Video {
-
-}
-
-export class Location {
-
-}
-
-export class Remote {
-
-}
-
-export class SearchResult {
-
-}
-
-export class Deleted {
-
-}
-
-export class SpecialFolder {
+export class SearchResult extends Facet {
   constructor(data: any) {
+    super(data);
 
-  } 
+  }
 }
 
-export class Quota {
+export class Deleted extends Facet {
+  constructor(data: any) {
+    super(data);
+
+  }
+}
+
+export class SpecialFolder extends Facet {
+  constructor(data: any) {
+    super(data);
+
+  }
+}
+
+export class Quota extends Facet {
   total: number;
   used: number;
   remaining: number;
@@ -58,14 +110,13 @@ export class Quota {
   state: string; // "normal | nearing | critical | exceeded"
 
   constructor(data: any) {
-    this.total = data.total;
-    this.used = data.used;
-    this.remaining = data.remaining;
-    this.deleted = data.deleted;
-    this.state = data.state;
+    super(data);
   }
 }
 
-export class Shared {
+export class Shared extends Facet {
+  constructor(data: any) {
+    super(data);
 
+  }
 }
--- src/resources.ts
+++ src/resources.ts
@@ -63,9 +63,9 @@ export class Item {
   eTag: string;
   cTag: string;
   createdBy: IdentitySet;
-  createdDateTime: string; // timestamp
+  createdDateTime: Date; // timestamp
   lastModifiedBy: IdentitySet;
-  lastModifiedDateTime: string;  // timestamp
+  lastModifiedDateTime: Date;  // timestamp
   size: number; // max 1024
   webUrl: string; // url
   description: string;
@@ -93,18 +93,23 @@ export class Item {
   constructor(data: any) {
     if (data.cTag) this.cTag = data.cTag; //: "adDpBMkRGNDYyOUY0OUI3NTFEITEwNi42MzU4OTA0ODQ1MTE1MzAwMDA"
     if (data.createdBy) this.createdBy = new IdentitySet(data.createdBy); //: {user: {displayName: "Luca Cossaro", id: "a2df4629f49b751d",…}}
-    if (data.createdDateTime) this.createdDateTime = data.createdDateTime; //: "2012-03-02T17:59:11.507Z"
+    if (data.createdDateTime) this.createdDateTime = new Date(data.createdDateTime); //: "2012-03-02T17:59:11.507Z"
     if (data.eTag) this.eTag = data.eTag; //: "aQTJERjQ2MjlGNDlCNzUxRCExMDYuMA"
     if (data.fileSystemInfo) this.fileSystemInfo = new Facets.FileSystemInfo(data.fileSystemInfo); //: {createdDateTime: "2012-03-02T17:59:11.507Z", lastModifiedDateTime: "2012-03-02T17:59:11.507Z"}
     if (data.folder) this.folder = new Facets.Folder(data.folder); //: {childCount: 52}
     if (data.id) this.id = data.id; //: "A2DF4629F49B751D!106"
     if (data.lastModifiedBy) this.lastModifiedBy = new IdentitySet(data.lastModifiedBy); //: {user: {displayName: "Luca Cossaro", id: "a2df4629f49b751d",…}}
-    if (data.lastModifiedDateTime) this.lastModifiedDateTime = data.lastModifiedDateTime; //: "2016-01-22T08:34:11.153Z"
+    if (data.lastModifiedDateTime) this.lastModifiedDateTime = new Date(data.lastModifiedDateTime); //: "2016-01-22T08:34:11.153Z"
     if (data.name) this.name = data.name; //: "Documenti"
     if (data.parentReference) this.parentReference = new ItemReference(data.parentReference); //: {driveId: "a2df4629f49b751d", id: "A2DF4629F49B751D!103", path: "/drive/root:"}
     if (data.size) this.size = data.size; //: 770409237
     if (data.specialFolder) this.specialFolder = new Facets.SpecialFolder(data.specialFolder); //: {name: "documents"}
     if (data.webUrl) this.webUrl = data.webUrl; //: "https://onedrive.live.com/redir?resid=A2DF4629F49B751D!106"
+    //@content.downloadUrl: "https://public-dm2306.files.1drv.com/y3myNN8BXViuCI8JxIsxOcwk-kdYbkRGslVIbEEA_YShCIB4KKKakq8_V9LqA-5Qrd3WUi0pKrktdXflGuD13C7OqhaT_LMYdAY33a_UxHcRs3qELaVWA6FlhrDiHVGiX6ijmc9ZZ6AdejbYlaxAHVU7lUsa-uDnezRsisXJOfFW6vp5QmyE77E3YUnwmsQpLGt"
+    if (data.file) this.file = new Facets.File(data.file); // {hashes: {crc32Hash: "00E4CB4B", sha1Hash: "64F2E0B8F9814A83B97CCC8DE754C91FD591BF52"},…}
+    if (data.location) this.location = new Facets.Location(data.location); //: {altitude: 0, latitude: 45.4562, longitude: 12.0287}
+    if (data.photo) this.photo = new Facets.Photo(data.photo); //: {takenDateTime: "2015-12-01T18:35:07Z"}
+    if (data.video) this.video = new Facets.Video(data.video); //: {bitrate: 20188536, duration: 69875, height: 1920, width: 1080}
   }
 
   get children() {
